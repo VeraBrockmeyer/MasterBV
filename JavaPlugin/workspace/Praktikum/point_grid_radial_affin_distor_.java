e5a2e96da825ac9b9fa8822345e06dbfd9d6c029
artjom <artjom.schwabski@smail.th-koeln.de>
2017-08-16 16:07:00 +0200

    Affine entzerrung

--- JavaPlugin/workspace/Praktikum/point_grid_radial_affin_distor_.java
+++ JavaPlugin/workspace/Praktikum/point_grid_radial_affin_distor_.java
@@ -7,19 +7,27 @@ import java.util.List;
 import org.apache.commons.math3.fitting.leastsquares.LeastSquaresBuilder;
 import org.apache.commons.math3.fitting.leastsquares.LeastSquaresOptimizer;
 import org.apache.commons.math3.fitting.leastsquares.LevenbergMarquardtOptimizer;
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.DecompositionSolver;
+import org.apache.commons.math3.linear.LUDecomposition;
+import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
 
 import ij.IJ;
 import ij.ImagePlus;
 import ij.WindowManager;
 import ij.gui.GenericDialog;
+import ij.gui.NewImage;
 import ij.plugin.filter.PlugInFilter;
 import ij.process.ImageProcessor;
 import ij.process.ShortProcessor;
 
+import PointPair;
+
 public class point_grid_radial_affin_distor_ implements PlugInFilter
 {
 	private ImagePlus sourcePicture;
-	private List<PointPair> PointPairs = new ArrayList<PointPair>();
 	private int nCol = 17;//19;
 	private int nRow = 11;
 	private int xCenter = 971;//1084;
@@ -29,8 +37,6 @@ public class point_grid_radial_affin_distor_ implements PlugInFilter
 	private int nYCross2Corner=5;
 	private ImagePlus debugImg;
 	private ImageProcessor debug;
-	private ArrayList<SimplePair> xPointPairs = new ArrayList<SimplePair>();
-	private ArrayList<SimplePair> yPointPairs = new ArrayList<SimplePair>();
 
 	
 	@SuppressWarnings("deprecation")
@@ -62,6 +68,10 @@ public class point_grid_radial_affin_distor_ implements PlugInFilter
 			choise = gd.getNextChoice();
 			
 			//Punktpaare einlesen:
+			List<PointPair> PointPairs = new ArrayList<PointPair>();
+			ArrayList<SimplePair> xPointPairs = new ArrayList<SimplePair>();
+			ArrayList<SimplePair> yPointPairs = new ArrayList<SimplePair>();
+			
 		  	for(int i=0; i < non_img_windows.length; i++)
 			{
 				if(((java.awt.Frame)non_img_windows[i]).getTitle() == choise)
@@ -114,14 +124,15 @@ public class point_grid_radial_affin_distor_ implements PlugInFilter
 //						debug.setColor(Color.GRAY);
 //						debug.drawString(""+xPair.index, (int)xPair.source, (int)yPair.source);
 //						debug.setColor(Color.WHITE);
-						debug.drawOval((int)xPair.target, (int)yPair.target, 3, 3);
+//						debug.drawOval((int)xPair.target, (int)yPair.target, 3, 3);
 					}
 					
 				}
 			}		
 		  	    
-   			ImagePlus debugImg = new ImagePlus("Debug",debug);
-  			debugImg.show();
+//   			ImagePlus debugImg = new ImagePlus("Debug",debug);
+//  			debugImg.show();
+		  	
 			//radiale entzerrung berechnen
 			double[] koeff = entzerungskoeefizienten_radial_berechnen(PointPairs);
 			double[] y_koeff= entzerungskoeefizienten_radial_berechnen_simple(yPointPairs);
@@ -136,9 +147,10 @@ public class point_grid_radial_affin_distor_ implements PlugInFilter
 			{
 				for (int x_target= 0; x_target < sourcePicture.getWidth(); x_target++)
 				{
+					// x_target / (1+ a*r^2 + b*r^4 * c*r^6) = x_distorted(source)
 
 					double radius2Center =computeRadius2Center(x_target, y_target);
-					// x_target / (1+ a*r^2 + b*r^4 * c*r^6) = x_distorted(source)
+					
 					
 //					double x_distorted =  (1./ (1. 
 //		        			+koeff[0] * Math.pow(radius2Center, 2.00) 
@@ -172,19 +184,45 @@ public class point_grid_radial_affin_distor_ implements PlugInFilter
 					}
 				}	
 			}
-			sp.setColor(Color.WHITE);
-			sp.setLineWidth(3);
-			for (PointPair p : PointPairs) {
-				sp.drawOval((int)p.x_target, (int)p.y_target, 3, 3);
-
-			}
+			
+			//Zeichne ziel punkte:
+//			sp.setColor(Color.WHITE);
+//			sp.setLineWidth(3);
+//			for (PointPair p : PointPairs) 
+//			{
+//				sp.drawOval((int)p.x_target, (int)p.y_target, 3, 3);
+//
+//			}
+			
 			//Am Ende müssen Sie das neue Bild noch auf den Bildschirm bringen.
 			newImg = new ImagePlus("Result", sp);
 			newImg.show();
 			
-			
+			//
 			//Affine entzerrung berechnen
-			//this.entzerrungsmatrix_affin_berechnen(PointPairs);
+			//
+			
+			//Punkt berechnen nach radialer entzerrung berechnen:
+			for (PointPair p : PointPairs) 
+			{
+				double radius2Center = computeRadius2Center(p.x_source, p.y_source);
+				
+				p.x_source =  (1./ (1. 
+	        			+x_koeff[0] * Math.pow(radius2Center, 2.00) 
+	        			+x_koeff[1] * Math.pow(radius2Center, 4.00)
+	        			+x_koeff[2] * Math.pow(radius2Center, 6.00)
+	        			) * p.x_source);
+				
+				p.y_source = (1./(1. 
+    			+y_koeff[0] * Math.pow(radius2Center, 2.00) 
+    			+y_koeff[1] * Math.pow(radius2Center, 4.00)
+    			+y_koeff[2] * Math.pow(radius2Center, 6.00)
+    			) * p.y_source);	
+						
+			}
+			
+			//Bild anhand der PunktPaare affin entzerren
+			this.entzerrungsmatrix_affin_berechnen(PointPairs, newImg);
 			
 		} 
 		catch(Exception exc)
@@ -193,7 +231,7 @@ public class point_grid_radial_affin_distor_ implements PlugInFilter
 		}			
 	}
 	
-	private double computeRadius2Center(int x_target, int y_target) {
+	private double computeRadius2Center(double x_target, double y_target) {
 	return	Math.sqrt((x_target - xCenter) * (x_target - xCenter)
 				+ (y_target - yCenter) * (y_target - yCenter));
 		
@@ -310,118 +348,126 @@ public class point_grid_radial_affin_distor_ implements PlugInFilter
 	
 	
 	//Berechnet anhand der Punktpaare die Affine Verzerrungsmatrix und gibt diese zurück
-//	private double[][] entzerrungsmatrix_affin_berechnen(List<PointPair> punkt_paare)
-//	{
-//		///Verschiebungsvektor und matrix berechnen
-//	    
-//		//Minimum von B*p -c;
-//		double[][] B = new double[2 * punkt_paare.size()][6];//Matrix B = koordinaten vorlage
-//		//double[] p;//p = geschter verschiebungsmatrix und vektor
-//		double[] c = new double[2 * punkt_paare.size()];//c = koordinaten transformiert
-//	    
-//	    //konstruiert man die (2n,6) -Matrix
-//		for(int i = 0; i< punkt_paare.size(); i++)
-//		{
-//			//i = 0 => 0,1 => i *2, i*2+1
-//			//i = 1 => 2,3
-//			//i = 2 => 4,5
-//			//i = 3 => 6,7
-//			
-//			//
-//			c[i*2] = punkt_paare.get(i).x_target;//x_i'
-//			c[i*2+1] = punkt_paare.get(i).y_target; //y_i'
-//			
-//			B[i*2][0] = punkt_paare.get(i).x_source; //x_i
-//			B[i*2][1] = punkt_paare.get(i).y_source; //y_i
-//			B[i*2][2] = 1;
-//			B[i*2][3] = 0;
-//			B[i*2][4] = 0;
-//			B[i*2][5] = 0;
-//			
-//			B[i*2+1][0] = 0;
-//			B[i*2+1][1] = 0;
-//			B[i*2+1][2] = 0;
-//			B[i*2+1][3] = punkt_paare.get(i).x_source;  //x_i
-//			B[i*2+1][4] = punkt_paare.get(i).y_source; //y_i
-//			B[i*2+1][5] = 1;
-//		}
-//	    
-//		//B*p = c => p = (Bt * B)^-1 * Bt * c
-//		
-//		RealVector c_vec = new ArrayRealVector(c, false);
-//		RealMatrix B_mat = MatrixUtils.createRealMatrix(B);
-//		
-//		//Bt
-//		RealMatrix B_mat_transp = B_mat.transpose();
-//		
-//		//Bt * B
-//		RealMatrix Bt_B = B_mat_transp.multiply(B_mat);
-//		
-//		//(Bt * B)^-1
-//		RealMatrix Bt_B_inv = MatrixUtils.inverse(Bt_B);
-//		
-//		//(Bt * B)^-1 * Bt * c
-//		RealVector p = Bt_B_inv.operate(B_mat_transp.operate(c_vec));
-//		
-//		double[] verschiebungsmatrix = new double[]{ p.getEntry(0), p.getEntry(1), p.getEntry(3), p.getEntry(4) };
-//		double[] verschiebungsvektor = new double[]{ p.getEntry(2), p.getEntry(5)};
-//		double[][] verzerrungsmatrix = {{verschiebungsmatrix[0], verschiebungsmatrix[1], verschiebungsvektor[0]}, {verschiebungsmatrix[2], verschiebungsmatrix[3], verschiebungsvektor[1]}, {0,0,1} };
-//		   
-//		IJ.log("a11: " + verschiebungsmatrix[0] + " a12: " + verschiebungsmatrix[1] + " a21: " + verschiebungsmatrix[2] + " a22: " + verschiebungsmatrix[3] + " t1: " + verschiebungsvektor[0] + " t2: " + verschiebungsvektor[1]);
-//		
-//		//Berechnen Sie aus den Werten die Umkehrabbildung, 
-//		
-//		//erzeugen Sie ein neues Bild gleicher Größe wie das Eingabebild 
-//		ImagePlus newImg;
-//		if(sourcePicture.getBitDepth() <= 8)
-//		{
-//			newImg =  NewImage.createByteImage ("entzertes_Bild", sourcePicture.getWidth(), sourcePicture.getHeight(), 1, 
-//			NewImage.FILL_WHITE); 
-//		}
-//		else
-//		{
-//			newImg =  NewImage.createRGBImage ("entzertes_Bild", sourcePicture.getWidth(), sourcePicture.getHeight(), 1, 
-//			NewImage.FILL_WHITE); 
-//		}
-//		
-//		
-//		//berechnen Sie es nach dem Target-to-Source-Verfahren, 
-//		//d.h.berechnen Sie zu jedem Punkt im neuen Bild durch die Umkehrabbildung die Koordinaten im
-//		//Eingangsbild und holen Sie von dort den Wert des Pixels. Eine Interpolation wird nicht verlangt.
-//		
-//		RealMatrix Bi_mat = MatrixUtils.createRealMatrix(verzerrungsmatrix);
-//	    DecompositionSolver Bi_mat_solver = new LUDecomposition(Bi_mat).getSolver();
-//	    
-//	    //zielbild durchlaufen und werde aus vorlagebild abrufen
-//        for (int y= 0; y < newImg.getHeight();y++) 
-//		{
-//			for (int x= 0; x < newImg.getWidth(); x++)
-//			{
-//				double[] t_h = {x,y,1};
-//				RealVector t_vec = new ArrayRealVector(t_h, false);
-//				RealVector coord_vec = Bi_mat_solver.getInverse().operate(t_vec); //Transformation berechnen
-//				
-//				int x_coord_vorlage =  (int) Math.round(coord_vec.getEntry(0));
-//				int y_coord_vorlage =  (int) Math.round(coord_vec.getEntry(1));
-//				
-//				if(x_coord_vorlage < sourcePicture.getWidth() && y_coord_vorlage < sourcePicture.getHeight())
-//				{
-//					newImg.getStack().getProcessor(1).putPixel(x, y, sourcePicture.getStack().getProcessor(1).getPixel(x_coord_vorlage, y_coord_vorlage));
-//				}
-//				else //mit schwarz auffüllen				
-//				{
-//					newImg.getStack().getProcessor(1).putPixel(x, y, 0);
-//				}			
-//			}
-//		}
-//		
-//		
-//		//Am Ende müssen Sie das neue Bild noch auf den Bildschirm bringen.
-//		newImg.show();
-//		
-//		return verzerrungsmatrix;
-//		
-//	}
+	private double[][] entzerrungsmatrix_affin_berechnen(List<PointPair> punkt_paare, ImagePlus radial_distorted_pic)
+	{
+		///Verschiebungsvektor und matrix berechnen
+	    
+		//Minimum von B*p -c;
+		double[][] B = new double[2 * punkt_paare.size()][6];//Matrix B = koordinaten vorlage
+		//double[] p;//p = geschter verschiebungsmatrix und vektor
+		double[] c = new double[2 * punkt_paare.size()];//c = koordinaten transformiert
+	    
+	    //konstruiert man die (2n,6) -Matrix
+		for(int i = 0; i< punkt_paare.size(); i++)
+		{
+			//i = 0 => 0,1 => i *2, i*2+1
+			//i = 1 => 2,3
+			//i = 2 => 4,5
+			//i = 3 => 6,7
+			
+			//
+			c[i*2] = punkt_paare.get(i).x_target;//x_i'
+			c[i*2+1] = punkt_paare.get(i).y_target; //y_i'
+			
+			B[i*2][0] = punkt_paare.get(i).x_source; //x_i
+			B[i*2][1] = punkt_paare.get(i).y_source; //y_i
+			B[i*2][2] = 1;
+			B[i*2][3] = 0;
+			B[i*2][4] = 0;
+			B[i*2][5] = 0;
+			
+			B[i*2+1][0] = 0;
+			B[i*2+1][1] = 0;
+			B[i*2+1][2] = 0;
+			B[i*2+1][3] = punkt_paare.get(i).x_source;  //x_i
+			B[i*2+1][4] = punkt_paare.get(i).y_source; //y_i
+			B[i*2+1][5] = 1;
+		}
+	    
+		//B*p = c => p = (Bt * B)^-1 * Bt * c
+		
+		RealVector c_vec = new ArrayRealVector(c, false);
+		RealMatrix B_mat = MatrixUtils.createRealMatrix(B);
+		
+		//Bt
+		RealMatrix B_mat_transp = B_mat.transpose();
+		
+		//Bt * B
+		RealMatrix Bt_B = B_mat_transp.multiply(B_mat);
+		
+		//(Bt * B)^-1
+		RealMatrix Bt_B_inv = MatrixUtils.inverse(Bt_B);
+		
+		//(Bt * B)^-1 * Bt * c
+		RealVector p = Bt_B_inv.operate(B_mat_transp.operate(c_vec));
+		
+		double[] verschiebungsmatrix = new double[]{ p.getEntry(0), p.getEntry(1), p.getEntry(3), p.getEntry(4) };
+		double[] verschiebungsvektor = new double[]{ p.getEntry(2), p.getEntry(5)};
+		double[][] verzerrungsmatrix = {{verschiebungsmatrix[0], verschiebungsmatrix[1], verschiebungsvektor[0]}, {verschiebungsmatrix[2], verschiebungsmatrix[3], verschiebungsvektor[1]}, {0,0,1} };
+		   
+		IJ.log("a11: " + verschiebungsmatrix[0] + " a12: " + verschiebungsmatrix[1] + " a21: " + verschiebungsmatrix[2] + " a22: " + verschiebungsmatrix[3] + " t1: " + verschiebungsvektor[0] + " t2: " + verschiebungsvektor[1]);
+		
+		//Berechnen Sie aus den Werten die Umkehrabbildung, 
+		
+		//erzeugen Sie ein neues Bild gleicher Größe wie das Eingabebild 
+		ImageProcessor processor;
+		if(radial_distorted_pic.getProcessor() instanceof ShortProcessor)
+		{
+			processor = new ShortProcessor(sourcePicture.getWidth(), sourcePicture.getHeight(), true);
+		}
+		else //other types to be implemented
+		{
+			return null;
+		}
+		
+		
+		
+		//berechnen Sie es nach dem Target-to-Source-Verfahren, 
+		//d.h.berechnen Sie zu jedem Punkt im neuen Bild durch die Umkehrabbildung die Koordinaten im
+		//Eingangsbild und holen Sie von dort den Wert des Pixels. Eine Interpolation wird nicht verlangt.
+		
+		RealMatrix Bi_mat = MatrixUtils.createRealMatrix(verzerrungsmatrix);
+	    DecompositionSolver Bi_mat_solver = new LUDecomposition(Bi_mat).getSolver();
+	    
+	    //zielbild durchlaufen und werde aus vorlagebild abrufen
+        for (int y= 0; y < processor.getHeight();y++) 
+		{
+			for (int x= 0; x < processor.getWidth(); x++)
+			{
+				double[] t_h = {x,y,1};
+				RealVector t_vec = new ArrayRealVector(t_h, false);
+				RealVector coord_vec = Bi_mat_solver.getInverse().operate(t_vec); //Transformation berechnen
+				
+				int x_coord_vorlage =  (int) Math.round(coord_vec.getEntry(0));
+				int y_coord_vorlage =  (int) Math.round(coord_vec.getEntry(1));
+				
+				if(x_coord_vorlage < radial_distorted_pic.getWidth() && y_coord_vorlage < radial_distorted_pic.getHeight())
+				{
+					processor.putPixel(x, y, (int)Math.round(radial_distorted_pic.getProcessor().getInterpolatedPixel(x_coord_vorlage, y_coord_vorlage)));
+				}
+				else //mit schwarz auffüllen				
+				{
+					processor.putPixel(x, y, 0);
+				}			
+			}
+		}
+		
+      //Zeichne ziel punkte:
+        processor.setColor(Color.BLACK);
+        processor.setLineWidth(3);
+		for (PointPair point : punkt_paare) 
+		{
+			processor.drawOval((int)point.x_target, (int)point.y_target, 3, 3);
+
+		}
+		
+		//Am Ende müssen Sie das neue Bild noch auf den Bildschirm bringen.
+        ImagePlus newImg = new ImagePlus("Affin Result", processor);
+		newImg.show();
+		
+		return verzerrungsmatrix;
+		
+	}
 
 	@Override
 	public int setup(String arg0, ImagePlus arg1) 
